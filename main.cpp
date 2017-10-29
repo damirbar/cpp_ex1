e71ca056966b8a70165b93c1a0c2cc8350cb3481
DESKTOP-D5S5QEJ\damir <damirbar@gmail.com>
2017-10-29 22:16:40 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 29, 2017
+ *      Author: damir
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	cout << "TESTINGGG" << endl;
+
+	return 0;
+}
+
