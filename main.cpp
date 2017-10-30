4e5a25361291d012d3cac67c454f6a69d56cc684
DESKTOP-D5S5QEJ\damir <damirbar@gmail.com>
2017-10-30 17:16:15 +0200

    - Major progress

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ protected:
 public:
 	Worker(string name, long id, double salary)
 	: _name(name), _id(id), _salary(salary){}
-	//TODO
+
 	Worker(const Worker &other)
 	: _name(other._name), _id(other._id), _salary(other._salary){}
 
@@ -42,7 +42,7 @@ public:
 
 
 
-/*                Manager                 */
+/*               Manager                 */
 
 
 class Manager : public Worker {
@@ -51,24 +51,27 @@ class Manager : public Worker {
 //	vector<Worker> workerList;
 
 public:
-	vector<Worker*> workerList;
+	vector<Worker> workerList;
 
 	Manager(string name, long id, double salary)
 	: Worker(name, id, salary) {}
 
 	Manager(const Manager &other)
-	: Worker(other._name, other._id, other._salary){}
+	: Worker(other._name, other._id, other._salary) {
+		for (auto iter : other.workerList) {
+			addWorker(iter);
+		}
+	}
 
 
 	bool addWorker(const Worker &w) {
-		Worker* tmp = new Worker(w);
-		workerList.push_back(tmp);
+		workerList.push_back(w);
 		return true;
 	}
 
 	bool removeWorker(const Worker &w) {
 		for (auto iter = workerList.begin(); iter != workerList.end(); ++iter) {
-			if (*iter == &w) {
+			if (*iter == w) {
 				workerList.erase(iter);
 				return true;
 			}
@@ -77,11 +80,10 @@ public:
 	}
 
 	const string toString() const {
-		cout << "\nINSIDE MANAGER TOSTRING. " << workerList.size() << " WORKERS" << endl;
 		string ans1 = Worker::toString() + ", Worker list:\n\n";
 		for (auto iter : workerList) {
-			ans1 += "\t- ";
-			ans1 += iter->toString() + "\n";
+			ans1 += "\t\t- ";
+			ans1 += iter.toString() + "\n";
 		}
 		const string ans = "" + ans1;
 		return ans;
@@ -103,7 +105,7 @@ public:
 
 class CEO : public Manager {
 
-	vector<Manager*> managerList;
+	vector<Manager> managerList;
 
 public:
 	CEO(string name, long id, double salary)
@@ -113,20 +115,13 @@ public:
 	: CEO(other._name, other._id, other._salary){}
 
 	bool addManager(const Manager &m) {
-//		Manager copy = Manager(m);
-//		for (auto iter = m.workerList.begin(); iter != m.workerList.end(); ++iter) {
-//			copy.addWorker(*iter);
-//		}
-//		cout << "Added a manager with " << copy.workerList.size() << " workers." << endl;
-//		managerList.push_back(&copy);
-		Manager* tmp = new Manager(m);
-		managerList.push_back(tmp);
+		managerList.push_back(m);
 		return true;
 	}
 
 	bool removeManager(const Manager &m) {
 		for (auto iter = managerList.begin(); iter != managerList.end(); ++iter) {
-			if (**iter == m) {
+			if (*iter == m) {
 				managerList.erase(iter);
 				return true;
 			}
@@ -138,7 +133,7 @@ public:
 		string ans1 = Worker::toString() + ", Manager list:\n\n";
 		for (auto iter : managerList) {
 			ans1 += "\t* ";
-			ans1 += iter->toString() + "\n";
+			ans1 += iter.toString() + "\n";
 		}
 		const string ans = "" + ans1;
 		return ans;
@@ -150,30 +145,43 @@ public:
 
 int main(){
 
+	const string breaker = "----------------------------------------------------------------";
+
 	Worker w1("WorkerA", 1, 1510);
 	Worker w2("WorkerB", 2, 1300);
 	Worker w3("WorkerC", 3, 1550);
 	Worker w4("WorkerD", 4, 1600);
 	Worker w5("WorkerE", 5, 1500);
+	Worker wc(w1);
 
 	Manager m1("ManagerA", 2345, 15000);
-	Manager m2("ManagerB", 3456, 15000);
 	m1.addWorker(w1);
 	m1.addWorker(w2);
 	m1.addWorker(w3);
+
+	Manager m2("ManagerB", 3456, 15000);
 	m2.addWorker(w4);
 	m2.addWorker(w5);
 
 	CEO c1("CEO-A", 11346, 30000);
 	c1.addManager(m1);
 	c1.addManager(m2);
-//	cout << w1.toString() << '\n' << endl;
-//	cout << m1.toString() << endl;
-//
-//	m1.removeWorker(w2);
-	cout << m1.toString() << endl;
 
-//	cout << c1.toString() << endl;
+	cout << "Worker w1 toString:" << endl;
+	cout << w1.toString() << '\n' << breaker << endl;
+
+	cout << "Worker wc, which is a copy of w1, toString:" << endl;
+	cout << wc.toString() << '\n' << breaker << endl;
+
+	cout << "Manager m1 toString:" << endl;
+	cout << m1.toString() << breaker << '\n' << endl;
+
+	cout << "Removing Worker w2 from Manager m1 and applying toString:" << '\n' << endl;
+	m1.removeWorker(w2);
+	cout << m1.toString() << breaker << endl;
+
+	cout << "CEO c1 toString:" << endl;
+	cout << c1.toString() << endl;
 
 	return 0;
 }
