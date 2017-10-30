8b709071209ce27fbef0802987fb4d626e829e25
DESKTOP-D5S5QEJ\damir <damirbar@gmail.com>
2017-10-30 11:57:51 +0200

    - Manager progress. - Started CEO

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ using namespace std;
 
 
 class Worker {
+
+protected:
 	string _name;
 	long _id;
 	double _salary;
@@ -22,59 +24,76 @@ public:
 	Worker(string name, long id, double salary)
 	: _name(name), _id(id), _salary(salary){}
 	//TODO
-//	Worker(const Worker &other) {
-//		if (*this != other) {
-//			_name = other._name;
-//			_id = other._id;
-//			_salary = other._salary;
-//		}
-//			return this;
-//	}
+	Worker(const Worker &other)
+	: _name(other._name), _id(other._id), _salary(other._salary){}
+
 	virtual const string toString() const {
 		const string ans = "Name: " + _name + ", ID: " + to_string(_id) + ", Salary: " + to_string(_salary);
 		return ans;
 	}
 
+	inline bool operator==(const Worker& rhs) {
+		return this->_name == rhs._name && this->_id == rhs._id && this->_salary == rhs._salary;
+	}
+
 	virtual ~Worker(){}
 };
 
-class Manager : public Worker {
 
-	vector<Worker> _wv;
+
+
+
+class Manager : public Worker {
+	vector<Worker> workerList;
 
 public:
 	Manager(string name, long id, double salary)
 	: Worker(name, id, salary) {}
 
-	//TODO
-//	Manager(const Manager &other) {
-//
-//	}
+	Manager(const Manager &other)
+	: Worker(other._name, other._id, other._salary){}
 
-	//TODO
+
 	bool addWorker(const Worker &w) {
-		_wv.push_back(w);
+		workerList.push_back(w);
 		return true;
 	}
-	//TODO
-//	bool removeWorker(const Worker &w) {
-//
-//	}
+
+	bool removeWorker(const Worker &w) {
+		for (auto iter = workerList.begin(); iter != workerList.end(); ++iter) {
+			if (*iter == w) {
+				workerList.erase(iter);
+				return true;
+			}
+		}
+		return false;
+	}
 
 	const string toString() const {
 		string ans1 = Worker::toString() + ", Worker list:\n\n";
 		int counter = 1;
-		for (auto iter : _wv) {
+		for (auto iter : workerList) {
 			ans1 += to_string(counter++) + ") ";
 			ans1 += iter.toString() + "\n";
 		}
-//		for (auto iter = _wv.begin(); iter != _wv.end(); ++iter) {
-//
-//		}
 		const string ans = "" + ans1;
 		return ans;
 	}
 
+};
+
+
+
+class CEO : public Manager {
+
+	vector<Manager> managerList;
+
+public:
+	CEO(string name, long id, double salary)
+	: Manager(name, id, salary) {}
+
+	CEO(const CEO &other)
+	: CEO(other._name, other._id, other._salary){}
 
 };
 
@@ -96,6 +115,9 @@ int main(){
 	m1.addWorker(w5);
 
 	cout << w1.toString() << '\n' << endl;
+	cout << m1.toString() << endl;
+
+	m1.removeWorker(w2);
 	cout << m1.toString() << endl;
 
 	return 0;
